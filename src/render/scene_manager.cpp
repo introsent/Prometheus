8c35525629bb63ac5afed2eb93e8a1da082a251f
introsent <161451906+introsent@users.noreply.github.com>
2025-10-27 16:03:08 +0100

    feat: create scene manager

---
+++ src/render/scene_manager.cpp
@@ -0,0 +1,62 @@
+﻿//
+// Created by minaj on 10/27/2025.
+//
+
+#include "scene_manager.h"
+
+#include "plane.h"
+#include "sphere.h"
+
+SceneManager::SceneManager() {
+    m_devicePtr = std::make_unique<EmbreeDevice>();
+    m_scenePtr = std::make_unique<EmbreeScene>(m_devicePtr.get());
+
+    // Default red material at index 0
+    m_materials.push_back(std::make_unique<Material_SolidColor>(colors::red));
+}
+
+SceneManager::~SceneManager() {
+    m_scenePtr->release();
+    m_devicePtr->release();
+}
+
+unsigned char SceneManager::addMaterial(Material* material) {
+    m_materials.push_back(std::unique_ptr<Material>(material));
+    return static_cast<unsigned char>(m_materials.size() - 1);
+}
+
+void SceneManager::addSphere(const glm::vec3& center, float radius, unsigned char materialId) {
+    auto sphere = std::make_unique<Sphere>(center, radius, m_devicePtr.get());
+    sphere->commit();
+    sphere->attach(m_scenePtr.get());
+
+    m_geometryMaterials.push_back(materialId);
+    m_geometries.push_back(std::move(sphere));
+}
+
+void SceneManager::addPlane(const glm::vec3& origin, const glm::vec3& normal, unsigned char materialId) {
+    auto plane = std::make_unique<Plane>(origin, normal, m_devicePtr.get());
+    plane->commit();
+    plane->attach(m_scenePtr.get());
+
+    m_geometryMaterials.push_back(materialId);
+    m_geometries.push_back(std::move(plane));
+}
+
+void SceneManager::commit() {
+    m_scenePtr->commit();
+}
+
+const Material* SceneManager::getMaterial(unsigned char materialId) const {
+    if (materialId < m_materials.size()) {
+        return m_materials[materialId].get();
+    }
+    return m_materials[0].get(); // Default to red
+}
+
+unsigned char SceneManager::getGeometryMaterial(unsigned int geomID) const {
+    if (geomID < m_geometryMaterials.size()) {
+        return m_geometryMaterials[geomID];
+    }
+    return 0; // Default material
+}
---
+++ src/render/scene_manager.h
@@ -0,0 +1,40 @@
+﻿//
+// Created by minaj on 10/27/2025.
+//
+
+#ifndef SCENE_MANAGER_H
+#define SCENE_MANAGER_H
+#include <memory>
+#include <vector>
+#include "embree_scene.h"
+#include "material.h"
+#include "embree4/rtcore_device.h"
+
+class EmbreeScene;
+class Geometry;
+class EmbreeDevice;
+
+class SceneManager {
+public:
+    SceneManager();
+    ~SceneManager();
+
+    unsigned char addMaterial(Material* material);
+    void addSphere(const glm::vec3& center, float radius, unsigned char materialId);
+    void addPlane(const glm::vec3& origin, const glm::vec3& normal, unsigned char materialId);
+
+    void commit();
+
+    RTCScene getScene() const { return m_scenePtr->handle(); }
+    const Material* getMaterial(unsigned char materialId) const;
+    unsigned char getGeometryMaterial(unsigned int geomID) const;
+
+private:
+    std::unique_ptr<EmbreeDevice> m_devicePtr;
+    std::unique_ptr<EmbreeScene> m_scenePtr;
+    std::vector<std::unique_ptr<Material>> m_materials;
+    std::vector<std::unique_ptr<Geometry>> m_geometries;
+    std::vector<unsigned char> m_geometryMaterials;
+};
+
+#endif //SCENE_MANAGER_H
