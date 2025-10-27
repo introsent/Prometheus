50f0dc9cee55f0214523b4f95511a0cf90396006
introsent <161451906+introsent@users.noreply.github.com>
2025-10-27 15:34:35 +0100

    feat: create base camera

---
+++ src/camera/camera.cpp
@@ -0,0 +1,31 @@
+﻿//
+// Created by minaj on 10/27/2025.
+//
+
+#include "camera.h"
+#include <cmath>
+
+#include "glm/ext/quaternion_geometric.hpp"
+
+Camera::Camera(const glm::vec3& position, float fovDegrees, float aspectRatio)
+    : m_position(position), m_aspectRatio(aspectRatio),
+      m_forward(0.f, 0.f, 1.f), m_right(1.f, 0.f, 0.f), m_up(0.f, 1.f, 0.f) {
+    setFOV(fovDegrees);
+}
+
+void Camera::setFOV(const float fovDegrees) {
+    const float fovRadians = fovDegrees * 3.14159265f / 180.f;
+    m_viewportHeight = 2.f * std::tan(fovRadians / 2.f);
+    m_viewportWidth = m_viewportHeight * m_aspectRatio;
+}
+
+Ray Camera::generateRay(const float u, const float v) const {
+    const float x = (u - 0.5f) * m_viewportWidth;
+    const float y = (0.5f - v) * m_viewportHeight; // Flip Y for screen coordinates
+
+    const glm::vec3 direction = glm::normalize(
+        m_forward + x * m_right + y * m_up
+    );
+
+    return Ray{m_position, direction};
+}
---
+++ src/camera/camera.h
@@ -0,0 +1,32 @@
+﻿//
+// Created by minaj on 10/27/2025.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+#include "glm/vec3.hpp"
+#include "../ray/ray.h"
+
+class Camera {
+public:
+    Camera(const glm::vec3& position, float fovDegrees, float aspectRatio);
+
+    [[nodiscard]] Ray generateRay(float u, float v) const;
+
+    void setPosition(const glm::vec3& pos) { m_position = pos; }
+    void setFOV(float fovDegrees);
+
+private:
+    glm::vec3 m_position;
+    glm::vec3 m_forward;
+    glm::vec3 m_right;
+    glm::vec3 m_up;
+    float m_aspectRatio;
+    float m_viewportHeight {};
+    float m_viewportWidth  {};
+};
+
+
+
+
+#endif //CAMERA_H
