a59f82b322c0e83dd6887f7ce162d861965bfbf6
introsent <161451906+introsent@users.noreply.github.com>
2025-10-27 12:34:26 +0100

    feat: add hit result struct

---
+++ src/ray/hit_result.h
@@ -0,0 +1,17 @@
+﻿//
+// Created by minaj on 10/27/2025.
+//
+
+#ifndef HIT_RESULT_H
+#define HIT_RESULT_H
+#include "embree4/rtcore_common.h"
+
+struct HitResult {
+    bool didHit = false;
+    float distance = 0.f;
+    unsigned geomID = RTC_INVALID_GEOMETRY_ID;
+    unsigned primID = 0;
+    float u = 0.f, v = 0.f;
+};
+
+#endif //HIT_RESULT_H
