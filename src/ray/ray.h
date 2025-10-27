82e648c6b29f670ef2076a89957b09ef74f207f6
introsent <161451906+introsent@users.noreply.github.com>
2025-10-27 12:32:43 +0100

    feat: add ray struct

---
+++ src/ray/ray.h
@@ -0,0 +1,17 @@
+﻿//
+// Created by minaj on 10/27/2025.
+//
+
+#ifndef RAY_H
+#define RAY_H
+#include "glm/vec3.hpp"
+
+struct Ray {
+    glm::vec3 origin {0.f, 0.f, -1.f};
+    glm::vec3 direction {0.f, 0.f, 1.f};
+    float tNear = 0.f;
+    float tFar = std::numeric_limits<float>::infinity();
+    unsigned int mask = 0xFFFFFFFF;
+};
+
+#endif //RAY_H
